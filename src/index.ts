4b93afb62a7ff3634b69fe63d6038d2486495ef4
Bartosz Lenartowicz <bartek.lenart95gmail.com>
2025-09-11 08:58:13 +1000

    Split to router

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+import router from "./router";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use( router);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
