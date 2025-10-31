480ee8f188b317c1265055d15d0a65bc0f2b8405
Bartosz Lenartowicz <bartek.lenart95@gmail.com>
2025-10-31 18:42:16 +1100

    Add eslint to API

--- api/src/libs/errorHandler.ts
+++ api/src/libs/errorHandler.ts
@@ -2,10 +2,10 @@ import { NextFunction, Request, Response } from "express";
 import multer from "multer";
 import { Logger } from "winston";
 import { ZodError } from "zod";
-
+// eslint-disable-next-line no-unused-vars
 export const ErrorHandler =
   (logger: Logger) =>
-  (error: any, req: Request, res: Response, next: NextFunction) => {
+  (error: Error, req: Request, res: Response, _: NextFunction) => {
     if (error instanceof multer.MulterError) {
       logger.error("multer error");
     } else if (error instanceof ZodError) {
